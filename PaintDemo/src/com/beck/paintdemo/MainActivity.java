691fbd56b63ae80d5ca73aac28d08acb1d239f4c
imakadmin <imakadmin@imakadmin-OptiPlex-755>
2012-11-30 10:44:15 -0600

    Added Hack for Showing Action Bar Overflow on Devices with Menu Button

--- PaintDemo/src/com/beck/paintdemo/MainActivity.java
+++ PaintDemo/src/com/beck/paintdemo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.beck.paintdemo;
 
+import java.lang.reflect.Field;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 
 public class MainActivity extends Activity {
      float x;
@@ -12,6 +15,25 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+         * HACK:  Show the overflow dots on devices
+         * that have a menu button
+         */
+           try {
+               ViewConfiguration config = ViewConfiguration.get(this);
+               Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+               if(menuKeyField != null) {
+                   menuKeyField.setAccessible(true);
+                   menuKeyField.setBoolean(config, false);
+               }
+           } catch (Exception ex) {
+               // Ignore
+           }
+           
+           /*
+            * END OVERFLOW HACK
+            *
+            */
     }
     @Override
     public boolean onTouchEvent(MotionEvent e)
